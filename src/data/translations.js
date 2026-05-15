48fed45a2fe918599b7fd0b01f502bf58b890fa4
Umugwaneza Aline <umugwanezaaline77@gmail.com>
2026-05-15 11:54:06 +0300

    data

---
+++ src/data/translations.js
@@ -0,0 +1,44 @@
+export const translations = {
+  en: {
+    home: "Home",
+    about: "About Us",
+    programs: "Programs",
+    getInvolved: "Get Involved",
+    donate: "Donate",
+    contact: "Contact Us",
+    story: "Our Story",
+    approach: "Our Approach",
+    team: "Our Team",
+    partner: "Be a Partner",
+    volunteer: "Be a Volunteer"
+  },
+  kiny: {
+    home: "Ahabanza",
+    about: "Abo turibo",
+    programs: "Ibikorwa",
+    getInvolved: "Fatanya Natwe",
+    donate: "Tanga Inkunga",
+    contact: "Twandikire",
+    story: "Amateka Yacu",
+    approach: "Imikorere Yacu",
+    team: "Ikipe Yacu",
+    partner: "Ba Umufatanyabikorwa",
+    volunteer: "Ba Umukorerabushake"
+  },
+  fr: {
+    home: "Accueil",
+    about: "À Propos",
+    programs: "Programmes",
+    getInvolved: "S'impliquer",
+    donate: "Faire un don",
+    contact: "Contactez-nous"
+  },
+  sw: {
+    home: "Nyumbani",
+    about: "Kuhusu Sisi",
+    programs: "Mipango",
+    getInvolved: "Shiriki",
+    donate: "Changia",
+    contact: "Wasiliana Nasi"
+  }
+};
